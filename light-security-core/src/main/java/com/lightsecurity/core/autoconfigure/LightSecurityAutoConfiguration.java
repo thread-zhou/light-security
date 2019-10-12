25bd0f9405eb8d218d10bc25ba9d5405de9c4613
zhoujian <thread_zhou@126.com>
2019-10-12 17:42:24 +0800

    添加LightSecurityAutoConfiguration类, 完成DelegatingFilterProxyRegistrationBean的创建

---
+++ light-security-core/src/main/java/com/lightsecurity/core/autoconfigure/LightSecurityAutoConfiguration.java
@@ -0,0 +1,52 @@
+package com.lightsecurity.core.autoconfigure;
+
+import com.lightsecurity.core.properties.SecurityProperties;
+import com.lightsecurity.core.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.DelegatingFilterProxyRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+public class LightSecurityAutoConfiguration {
+
+    private static final String DEFAULT_FILTER_NAME = AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME;
+
+    /**
+     * DelegatingFilterProxyRegistrationBean的作用是在springboot环境下通过内嵌容器(Tomcat等)启动类来注册一个DelegatingFilterProxy
+     * @param securityProperties
+     * @return
+     */
+    @Bean
+    @ConditionalOnBean(name = DEFAULT_FILTER_NAME)
+    public DelegatingFilterProxyRegistrationBean lightSecurityFilterChainRegistration(SecurityProperties securityProperties){
+        DelegatingFilterProxyRegistrationBean delegatingFilterProxyRegistrationBean = new DelegatingFilterProxyRegistrationBean(DEFAULT_FILTER_NAME);
+        delegatingFilterProxyRegistrationBean.setOrder(securityProperties.getFilterOrder());
+        delegatingFilterProxyRegistrationBean.setDispatcherTypes(getDispatcherTypes(securityProperties));
+        return delegatingFilterProxyRegistrationBean;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SecurityProperties securityProperties(){
+        return new SecurityProperties();
+    }
+
+    private EnumSet<DispatcherType> getDispatcherTypes(SecurityProperties securityProperties){
+        if (securityProperties.getFilterDispatcherTypes() == null){
+            return null;
+        }
+        Set<DispatcherType> dispatcherTypes = new HashSet<>();
+        for (String dispatcherType : securityProperties.getFilterDispatcherTypes()){
+            dispatcherTypes.add(DispatcherType.valueOf(dispatcherType));
+        }
+        return EnumSet.copyOf(dispatcherTypes);
+    }
+
+}
--- light-security-core/src/main/java/com/lightsecurity/core/properties/SecurityProperties.java
+++ light-security-core/src/main/java/com/lightsecurity/core/properties/SecurityProperties.java
@@ -1,10 +1,32 @@
 package com.lightsecurity.core.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
+import java.util.Set;
+
 
 /**
  * 核心配置类
  */
 @ConfigurationProperties(prefix = "light.security")
 public class SecurityProperties {
+
+    public static final int DEFAULT_FILTER_ORDER = FilterRegistrationBean.REQUEST_WRAPPER_FILTER_MAX_ORDER - 100;
+
+
+    private int filterOrder = DEFAULT_FILTER_ORDER;
+
+    /**
+     * Light security filter Chain dispatcher types
+     */
+    private Set<String> filterDispatcherTypes;
+
+    public int getFilterOrder() {
+        return filterOrder;
+    }
+
+    public Set<String> getFilterDispatcherTypes() {
+        return filterDispatcherTypes;
+    }
 }
